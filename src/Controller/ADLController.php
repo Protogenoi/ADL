99f8370f2b8740c98b93f7393a024c130835c2b3
Michael <michael@adl-crm.uk>
2018-08-22 14:50:05 +0100

    Basic setup

---
+++ src/Controller/ADLController.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * ------------------------------------------------------------------------
+ *                               ADL CRM
+ * ------------------------------------------------------------------------
+ *
+ * Copyright © 2018 ADL CRM All rights reserved.
+ *
+ * Unauthorised copying of this file, via any medium is strictly prohibited.
+ * Unauthorised distribution of this file, via any medium is strictly prohibited.
+ * Unauthorised modification of this code is strictly prohibited.
+ *
+ * Proprietary and confidential
+ *
+ * Written by michael <michael@adl-crm.uk>, 22/08/2018 14:23
+ *
+ * ADL CRM makes use of the following third party open sourced software/tools:
+ *  DataTables - https://github.com/DataTables/DataTables
+ *  EasyAutocomplete - https://github.com/pawelczak/EasyAutocomplete
+ *  PHPMailer - https://github.com/PHPMailer/PHPMailer
+ *  ClockPicker - https://github.com/weareoutman/clockpicker
+ *  fpdf17 - http://www.fpdf.org
+ *  summernote - https://github.com/summernote/summernote
+ *  Font Awesome - https://github.com/FortAwesome/Font-Awesome
+ *  Bootstrap - https://github.com/twbs/bootstrap
+ *  jQuery UI - https://github.com/jquery/jquery-ui
+ *  Google Dev Tools - https://developers.google.com
+ *  Twitter API - https://developer.twitter.com
+ *  Webshim - https://github.com/aFarkas/webshim/releases/latest
+ *
+ */
+
+namespace App\Controller;
+
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class ADLController extends AbstractController
+{
+
+    /**
+     * @Route("/")
+     */
+
+    public function loginPage()
+    {
+        return new Response('ADL Login Page');
+    }
+
+    /**
+     * @Route("/ADL/{slug}")
+     */
+
+    public function mainPage($slug)
+    {
+        return $this->render('ADL/mainPage.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+        ]);
+    }
+
+}
