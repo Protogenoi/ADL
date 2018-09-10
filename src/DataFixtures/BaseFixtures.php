e454ee890781f8ce56c7e3d7f833331cc00f92c4
Michael <michael@adl-crm.uk>
2018-09-10 18:00:01 +0100

    Added fixture to loop through when seeding dummy data.

---
+++ src/DataFixtures/BaseFixtures.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * ------------------------------------------------------------------------
+ *                               ADL CRM
+ * ------------------------------------------------------------------------
+ *
+ * Copyright © 2018 ADL CRM All rights reserved.
+ *
+ * Unauthorised copying of this file, via any medium is strictly prohibited.
+ * Unauthorised distribution of this file, via any medium is strictly prohibited.
+ * Unauthorised modification of this code is strictly prohibited.
+ *
+ * Proprietary and confidential
+ *
+ * Written by michael <michael@adl-crm.uk>, 10/09/2018 17:51
+ *
+ * ADL CRM makes use of the following third party open sourced software/tools:
+ *  DataTables - https://github.com/DataTables/DataTables
+ *  EasyAutocomplete - https://github.com/pawelczak/EasyAutocomplete
+ *  PHPMailer - https://github.com/PHPMailer/PHPMailer
+ *  ClockPicker - https://github.com/weareoutman/clockpicker
+ *  fpdf17 - http://www.fpdf.org
+ *  summernote - https://github.com/summernote/summernote
+ *  Font Awesome - https://github.com/FortAwesome/Font-Awesome
+ *  Bootstrap - https://github.com/twbs/bootstrap
+ *  jQuery UI - https://github.com/jquery/jquery-ui
+ *  Google Dev Tools - https://developers.google.com
+ *  Twitter API - https://developer.twitter.com
+ *  Webshim - https://github.com/aFarkas/webshim/releases/latest
+ *
+ */
+
+namespace App\DataFixtures;
+
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+abstract class BaseFixtures extends Fixture
+{
+
+    /** @var ObjectManager */
+    private $manager;
+
+    abstract protected function loadData(ObjectManager $em);
+
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+
+        $this->loadData($manager);
+    }
+
+    protected function createMany(string $className, int $count, callable $factory)
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $entity = new $className();
+            $factory($entity, $i);
+            $this->manager->persist($entity);
+
+            $this->addReference($className . '_' . $i, $entity);
+        }
+    }
+
+}
--- src/DataFixtures/ClientsFixtures.php
+++ src/DataFixtures/ClientsFixtures.php
@@ -3,29 +3,31 @@
 namespace App\DataFixtures;
 
 use App\Entity\Clients;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ClientsFixtures extends Fixture
+class ClientsFixtures extends BaseFixtures
 {
-    public function load(ObjectManager $manager)
+    public function loadData(ObjectManager $manager)
     {
 
-        $client = new Clients();
-        $client->setTitle('Mr')
-            ->setFirstName('Michael')
-            ->setLastName('Owen')
-            ->setDob(\DateTime::createFromFormat('Y-m-d', '1987-02-08'))
-            ->setPhoneNumber('07401434619')
-            ->setEmail('michael@adl-crm.uk')
-            ->setAddress1('12 Clos Y Cwm')
-            ->setTown('Pontardawe')
-            ->setPostcode('SA8 4NA')
-            ->setAddedBy('ADL')
-            ->setAddedDate(\DateTime::createFromFormat('Y-m-d', '2018-09-10'))
-            ->setOwner('E Corp');
 
-        $manager->persist($client);
+        $this->createMany(Clients::class, 10, function (Clients $client, $count) {
+
+            $client->setTitle('Mr')
+                ->setFirstName('Michael')
+                ->setLastName('Owen')
+                ->setDob(\DateTime::createFromFormat('Y-m-d', '1987-02-08'))
+                ->setPhoneNumber('07401434619')
+                ->setEmail('michael@adl-crm.uk')
+                ->setAddress1('12 Clos Y Cwm')
+                ->setTown('Pontardawe')
+                ->setPostcode('SA8 4NA')
+                ->setAddedBy('ADL')
+                ->setAddedDate(\DateTime::createFromFormat('Y-m-d',
+                    '2018-09-10'))
+                ->setOwner('E Corp');
+        });
+
         $manager->flush();
     }
 }
