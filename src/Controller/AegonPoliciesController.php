afc2fd927f0cc8a42e49ca4cb2f5ab29e6992a57
Michael <michael@adl-crm.uk>
2018-09-18 11:03:53 +0100

    Added base code for editing specific Aegon policy details.

--- src/Controller/AegonPoliciesController.php
+++ src/Controller/AegonPoliciesController.php
@@ -41,10 +41,29 @@ class AegonPoliciesController extends AbstractController
     /**
      * @Route("/aegon/edit/policies", name="edit_aegon_policies")
      */
-    public function editAegonPolicy()
+    public function editAegonPolicy(
+        Request $request,
+        EntityManagerInterface $entityManager
+    )
     {
+
+        $CID = $request->query->get('CID');
+        $PID = $request->query->get('PID');
+
+        $repository = $entityManager->getRepository(Policy::class);
+        /** @var  policy */
+        $policy = $repository->findOneBy(['id' => $PID, 'client' => $CID]);
+
+        if (!$policy) {
+            throw $this->createNotFoundException(sprintf('No policy found for CID:"%s" with PID:%s" found',
+                $CID, $PID));
+        }
+
         return $this->render('aegon_policies/edit_aegon_policy.html.twig', [
             'title' => 'Edit Aegon Policy',
+            'CID' => $CID,
+            'PID' => $PID,
+            'policy' => $policy,
         ]);
     }
 }
