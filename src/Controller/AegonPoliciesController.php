9731d84c9d504d91476454d8741789eefa539fb6
Michael <michael@adl-crm.uk>
2018-09-18 10:44:59 +0100

    Added base code for showing specific Aegon policy details.

--- src/Controller/AegonPoliciesController.php
+++ src/Controller/AegonPoliciesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Policy;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AegonPoliciesController extends AbstractController
@@ -10,10 +13,28 @@ class AegonPoliciesController extends AbstractController
     /**
      * @Route("/aegon/view/policies", name="view_aegon_policies")
      */
-    public function viewAegonPolicy()
-    {
+    public function viewAegonPolicy(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ) {
+
+        $CID = $request->query->get('CID');
+        $PID = $request->query->get('PID');
+
+        $repository = $entityManager->getRepository(Policy::class);
+        /** @var  policy */
+        $policy = $repository->findOneBy(['id' => $PID, 'client' => $CID]);
+
+        if (!$policy) {
+            throw $this->createNotFoundException(sprintf('No policy found for CID:"%s" with PID:%s" found',
+                $CID, $PID));
+        }
+
         return $this->render('aegon_policies/view_aegon_policy.html.twig', [
             'title' => 'View Aegon Policy',
+            'CID' => $CID,
+            'PID' => $PID,
+            'policy' => $policy,
         ]);
     }
 
