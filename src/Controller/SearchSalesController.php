fe54674cb7049264651cd30b54b1be6ee2af3a3a
Michael <michael@adl-crm.uk>
2018-09-20 15:53:35 +0100

    Added code to search sales by date range,

---
+++ src/Controller/SearchSalesController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PolicyRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchSalesController extends AbstractController
+{
+    /**
+     * @Route("/search/life/sales", name="search_sales")
+     */
+    public function index(
+        PolicyRepository $policyRepository,
+        Request $request,
+        PaginatorInterface $paginator
+    ) {
+        $datefrom = $request->query->get('datefrom');
+        $dateto = $request->query->get('dateto');
+
+        $queryBuilder
+            = $policyRepository->findAllPolicySalesWithSearch($datefrom,
+            $dateto);
+
+        $pagination = $paginator->paginate($queryBuilder,
+            $request->query->getInt('page', 1), 20);
+
+        return $this->render('search_sales/search_sales.html.twig', [
+            'title' => 'Search Sales',
+            'policy' => $pagination,
+        ]);
+    }
+}
