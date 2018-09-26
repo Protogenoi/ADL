9832ba1c037fbfc179e9dc4746cc3d68c9eaf051
Michael <michael@adl-crm.uk>
2018-09-26 15:13:10 +0100

    Added reports menu. Removed links from nav.

---
+++ src/Controller/LifeReportsMenuController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LifeReportsMenuController extends AbstractController
+{
+    /**
+     * @Route("/life/reports/menu", name="app_life_reports_menu")
+     */
+    public function lifeMenuController()
+    {
+        return $this->render('life_reports_menu/life_reports_menu.html.twig', [
+            'title' => 'Reports',
+        ]);
+    }
+}
